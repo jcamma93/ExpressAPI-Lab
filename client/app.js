53e8866e499a1f7de1275b4f797d8f1a000db8bf
Jesse Cammarano <jcamma93@gmail.com>
2023-07-10 09:38:11 -0400

    Lab Complete

--- client/app.js
+++ client/app.js
@@ -3,41 +3,60 @@
 const userInput = document.getElementById('user');
 const textInput = document.getElementById('text');
 
-document.getElementById("addChirp").addEventListener('click', async(e) => {
+document.getElementById("addChirp").addEventListener('click', async (e) => {
     e.preventDefault();
-    document.location.reload();
-    // console.log(userInput.value)
-    // console.log(textInput.value)
-    console.log({ user: userInput.value, text: textInput.value });
+    
     const res = await fetch("/api/chirps", {
         method: 'POST',
         headers: { "Content-Type": 'application/json' },
         body: JSON.stringify({ user: userInput.value, text: textInput.value }),
     });
-    getAllChirps();
-}
-);
+    if (res.ok) {
+        getAllChirps();
+        userInput.value = '';
+        textInput.value = '';
+    }
+});
 
 const chirpList = document.getElementById("chirplist");
 
+function displayChirps(data) {
+    chirpList.innerHTML = '';
+    data.forEach(chirp => {
+        const p = document.createElement('p');
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'X';
+        deleteButton.className = 'btn btn-sm btn-outline-danger';
+        p.id = chirp.id;
+        p.textContent = `${chirp.user} : ${chirp.text}`;
+        p.appendChild(deleteButton);
+        chirpList.appendChild(p);
+
+        deleteButton.addEventListener('click', async () => {
+            window.location.reload();
+            const res = await fetch(`/api/chirps/${chirp.id}`, {
+                method: 'DELETE'
+            });
+            if (res.ok) {
+                getAllChirps();
+
+            }
+        });
+    });
+}
+
 async function getAllChirps() {
     try {
-        const res = await fetch("/api/chirps");
+        const res = await fetch('/api/chirps');
         const data = await res.json();
         if (res.ok) {
-            chirpList.innerHTML = "";
-            data.forEach(chirp => {
-                const p = document.createElement("p");
-                p.id = chirp.id;
-                p.textContent = `${chirp.user} : ${chirp.text}`;
-                chirpList.appendChild(p);
-            });
+            displayChirps(data);
         } else {
             console.error(data.message);
         }
     } catch (error) {
         console.error(error);
-    };
-};
+    }
+}
 
 getAllChirps();
